d83364c24e42cf7cb08c9567e6fd7a062783c211
Kusano TOMOKI <kusano@mediowl.co.jp>
2020-03-12 11:09:01 +0900

    detail.blade.php の修正
    インデントの修正

--- resources/views/tasks/detail.blade.php
+++ resources/views/tasks/detail.blade.php
@@ -10,37 +10,31 @@
           </div>
           <div class="panel-body">
             <table class="table">
-                <tr>
-                  <td>Title</td>
-                  <td>{{ $task->title }}</td>
-                </tr>
-                <tr>
-                  <td>STATUS</td>
-                  <td>{{ $task->status_label }}</td>
-                </tr>
-
-                <tr>
-                  <td>DueDATE</td>
-                  <td>{{$task->formatted_due_date }}</td>
-                </tr>
-
-                <tr>
-                  <td>Created date</td>
-                  <td>{{ $task->created_at}}</td>
-                </tr>
-
-                <tr>
-                  <td>Updated date</td>
-                  <td>{{ $task->updated_at}}</td>
-                </tr>
-              <table>
+              <tr>
+                <td>Title</td>
+                <td>{{ $task->title }}</td>
+              </tr>
+              <tr>
+                <td>STATUS</td>
+                <td>{{ $task->status_label }}</td>
+              </tr>
+              <tr>
+                <td>DueDATE</td>
+                <td>{{$task->formatted_due_date }}</td>
+              </tr>
+              <tr>
+                <td>Created date</td>
+                <td>{{ $task->created_at }}</td>
+              </tr>
+              <tr>
+                <td>Updated date</td>
+                <td>{{ $task->updated_at }}</td>
+              </tr>
+            </table>
           </div>
         </div>
       </div>
-      <a href="{{ route('tasks.index',['id' => $folder_id])}}" class="btn btn-primary btn-sm">BACK</a>
+      <a href="{{ route('tasks.index', ['id' => $folder_id]) }}" class="btn btn-primary btn-sm">BACK</a>
     </div>
   </div>
-
-
-
 @endsection
