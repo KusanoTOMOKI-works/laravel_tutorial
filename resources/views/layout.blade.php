950b970748f4593f23c1a5bfe1231b616d1dab28
Kusano TOMOKI <kusano@mediowl.co.jp>
2020-03-12 12:11:48 +0900

    layout.blabe.php 編集
    インデント修正

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,18 +14,18 @@
     <a class="my-navbar-brand" href="/">ToDo App</a>
     <div class="my-navbar-control">
       @if(Auth::check())
-      <span class="my-navbar-item"> Welcome {{ Auth::user()->name}} さん</span>
-     |
-     <a href="#" id="logout" class="my-navbar-item">Logout</a>
-     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display : none;">
-       @csrf
-     </form>
-    @else
-    <a class="my-navbar-item" href="{{ route('login') }}"> Login </a>
-    |
-    <a class="my-navbar-item" href="{{route('register' )}}"> 会員登録 </a>
-    @endif
-  </div>
+        <span class="my-navbar-item"> Welcome {{ Auth::user()->name}} さん</span>
+        |
+        <a href="#" id="logout" class="my-navbar-item">Logout</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display : none;">
+          @csrf
+        </form>
+      @else
+        <a class="my-navbar-item" href="{{ route('login') }}"> Login </a>
+        |
+        <a class="my-navbar-item" href="{{route('register' )}}"> 会員登録 </a>
+      @endif
+    </div>
   </nav>
 </header>
 <main>
@@ -41,8 +41,6 @@
    });
 </script>
 @endif
-
-
 @yield('scripts')
 </body>
 </html>
