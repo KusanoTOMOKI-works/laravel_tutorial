d32d4885922c0c6b5987a8d83e0d1fd58d4ce29f
Kusano TOMOKI <kusano@mediowl.co.jp>
2020-03-12 11:32:04 +0900

    HomeController.php の修正
    インデントの修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,23 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-
 class HomeController extends Controller
 {
+    public function index()
+    {
+        $user =Auth::user();
+        $folder =$user->folders()->first();
 
-  public function index(){
-    $user =Auth::user();
+        if (is_null($folder)) {
+            return view('home');
+        }
 
-
-    $folder =$user->folders()->first();
-
-
-    if(is_null($folder)) {
-      return view('home');
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
     }
-
-    return redirect()->route('tasks.index',[
-      'id' => $folder->id,
-    ]);
-  }
 }
