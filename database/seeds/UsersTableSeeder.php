09fffbac6ca43d2aae00c9743c5413f248cad608
Kusano TOMOKI <kusano@mediowl.co.jp>
2020-03-13 12:26:33 +0900

    User のシーダー
    インデント修正

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,11 +13,11 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-        'name' =>         'test',
-        'email' =>        'dummy@email.com',
-        'password' =>     bcrypt('test1234'),
-        'created_at' =>  Carbon::now(),
-        'updated_at' =>   Carbon::now(),
+        'name'       => 'test',
+        'email'      => 'dummy@email.com',
+        'password'   => bcrypt('test1234'),
+        'created_at' => Carbon::now(),
+        'updated_at' => Carbon::now(),
       ]);
     }
 }
