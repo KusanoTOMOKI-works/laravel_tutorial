dddff7a32b306b1f37d66d0a1c5c2e66c5b6e1a0
Kusano TOMOKI <kusano@mediowl.co.jp>
2020-03-13 17:20:40 +0900

    FoldersTableSeeder の編集
    インデントの修正

--- database/seeds/FolderTableSeeder.php
+++ database/seeds/FolderTableSeeder.php
@@ -15,17 +15,17 @@ class FolderTableSeeder extends Seeder
 
     public function run()
     {
-      $user = DB::table('users')->first();
+        $user = DB::table('users')->first();
 
-      $titles = ['private', 'work', 'trip'];
+        $titles = ['private', 'work', 'trip'];
 
-      foreach ($titles as $title){
-        DB::table('folders')->insert([
-            'title'       =>  $title,
-            'user_id'     =>  $user->id,
-            'created_at'  =>  Carbon::now(),
-            'updated_at'  =>  Carbon::now()
-          ]);
+        foreach ($titles as $title){
+            DB::table('folders')->insert([
+                'title'      => $title,
+                'user_id'    => $user->id,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
         }
     }
 }
